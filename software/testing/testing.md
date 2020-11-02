9df82fafb35265f775a1a87e70f48d22d0c3a90f
Maeve Stites <maeve@mstites.com>
2020-11-01 21:23:17 -0500

    Update cam information

--- software/testing/testing.md
+++ software/testing/testing.md
@@ -12,7 +12,7 @@ Information on testing the functionality of the HUZZAH32, to help with final imp
 ### Example Files
 * [rgbWeb](https://diyusthad.com/2019/10/controlling-rgb-lights-from-esp32-web-server.html) - RGB control via web server
 * webControl - testing based of example files for moving towards iteration
-* [cam](https://randomnerdtutorials.com/esp32-cam-take-photo-display-web-server/) - Camera control via web server, featuring a neater way of handeling html data
+* [cam](https://randomnerdtutorials.com/esp32-cam-take-photo-display-web-server/) - Camera control via web server, featuring a neater way of handeling html data and javascript comm with the unit
 #### Built in Examples
 * WebUpdate - upload a new binary to load
 * SDWebServer - host web server from SD card with full webserver
--- software/testing/web/cam/cam.ino
+++ software/testing/web/cam/cam.ino
@@ -1,16 +1,3 @@
-/*********
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-cam-take-photo-display-web-server/
-  
-  IMPORTANT!!! 
-   - Select Board "AI Thinker ESP32-CAM"
-   - GPIO 0 must be connected to GND to upload a sketch
-   - After connecting GPIO 0 to GND, press the ESP32-CAM on-board RESET button to put your board in flashing mode
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*********/
-
 #include "WiFi.h"
 #include "esp_camera.h"
 #include "esp_timer.h"
@@ -25,8 +12,8 @@
 #include <FS.h>
 
 // Replace with your network credentials
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "GoofZone";
+const char* password = "9139122626";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -188,4 +175,47 @@ void loop() {
 
 // Check if photo capture was successful
 bool checkPhoto( fs::FS &fs ) {
-  File f_pic 
+  File f_pic = fs.open( FILE_PHOTO );
+  unsigned int pic_sz = f_pic.size();
+  return ( pic_sz > 100 );
+}
+
+// Capture Photo and Save it to SPIFFS
+void capturePhotoSaveSpiffs( void ) {
+  camera_fb_t * fb = NULL; // pointer
+  bool ok = 0; // Boolean indicating if the picture has been taken correctly
+
+  do {
+    // Take a photo with the camera
+    Serial.println("Taking a photo...");
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+
+    // Photo file name
+    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+
+    // Insert the data in the photo file
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.print("The picture has been saved in ");
+      Serial.print(FILE_PHOTO);
+      Serial.print(" - Size: ");
+      Serial.print(file.size());
+      Serial.println(" bytes");
+    }
+    // Close the file
+    file.close();
+    esp_camera_fb_return(fb);
+
+    // check if file has been correctly saved in SPIFFS
+    ok = checkPhoto(SPIFFS);
+  } while ( !ok );
+}
