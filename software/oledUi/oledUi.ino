47780a4f7fa445b920080416eabf1b48a966284a
mstites <maeve@mstites.com>
2020-12-01 20:31:49 -0500

    Add basic temp target control

--- software/oledUi/oledUi.ino
+++ software/oledUi/oledUi.ino
@@ -13,9 +13,9 @@
 /////////////////////////////////////////////////////////////////
 const int PLATE_POWER_PIN = 26;
 
-const int UP_BUTTON_PIN = A10;
+const int UP_BUTTON_PIN = A8;
 const int SELECT_BUTTON_PIN = A9;
-const int DOWN_BUTTON_PIN = A8;
+const int DOWN_BUTTON_PIN = A10;
 
 /////////////////////////////////////////////////////////////////
 // Instantiate objects  /////////////////////////////////////////
@@ -34,6 +34,10 @@ SSD1306AsciiWire oled;
 /////////////////////////////////////////////////////////////////
 
 boolean platePower = false; // pwr to relay controlling hotplate
+byte MODE = 1; // 1 -> select, 2 -> adjust target temp
+int SEL = 1; // selection
+int TARGET_TEMP = 130; // target temperature
+byte SCREEN = 0; // CURRENT SCREEN 0-STANDBY, 1-HEATING
 
 /////////////////////////////////////////////////////////////////
 
@@ -48,8 +52,9 @@ void setup() {
   Wire.setClock(400000l);
   oled.begin(&Adafruit128x64, I2C_ADDRESS);
   oled.setFont(System5x7);
-  displayStandby(1, 1);
-  
+  standbyScreen(1, 1, TARGET_TEMP);
+
+  delay(5000);
   // set button handlers
   up_button.setTapHandler(upPress);
   select_button.setTapHandler(selectPress);
@@ -72,38 +77,72 @@ void loop() {
 }
 
 /////////////////////////////////////////////////////////////////
-// State and Update Handler /////////////////////////////////////
+// Button Loops /////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
-byte MODE = 1; // 1 -> select, 2 -> adjust target temp
-byte SCREEN = 0; // screen number
-int SEL = 1; // selection
+void upPress(Button2& btn) {
+    Serial.println("Up: Pressed");
+    arrow(1);
+}
 
-void select(){
+void selectPress(Button2& btn) {
+    unsigned int time = btn.wasPressedFor();
+    if (time < 1000) {
+        Serial.println("Select: Regular press");
+        middle();
+        togglePlatePower();
+    } 
+    else {
+        Serial.println("Select: long press");
+        Serial.println("Turning OFF");
+        setPlatePower(false);
+    }
+}
+
+void downPress(Button2& btn) {
+    Serial.println("Down: Pressed");
+    arrow(-1);
+}
+
+/////////////////////////////////////////////////////////////////
+// Button Func/State Handler ////////////////////////////////////
+/////////////////////////////////////////////////////////////////
+
+// MIDDLE BUTTON
+void middle(){
   if (MODE == 1){ // SELECT
     if (SCREEN == 0) { // standby
       if (SEL == 1) {
-        // go to heating screen
+        changeScreen(1); // go to heating screen
       }
       else {
-        MODE == 2; // change target
+        Serial.println("Change target temp");
+        MODE = 2; // change target temp
       }
     }
+    else if (SCREEN == 1) { // heating
+      
+    }
   }
-
   else if (MODE == 2){ // ADJUST
-    // PRESS TO EXIT/SET
+    MODE = 1; // back to select mode
+    Serial.print("Back to select mode");
   }
 }
 
+// BUTTOM/TOP BUTTON FUNCTIONALITY
 void arrow(int dir){
   if (MODE == 1){ // SELECT
-    updateSelection(dir);
+    refreshScreen(dir, TARGET_TEMP);
   }
   else if (MODE == 2){ // ADJUST
-    // adjust value up or down
+    TARGET_TEMP = TARGET_TEMP + dir; // change target temp
+    refreshScreen(0, TARGET_TEMP); // no selection change
   }
+  Serial.println(SEL);
 }
+
+// CHANGE TO NEW SCREEN
 void changeScreen(byte newScreen){
   if (SCREEN != newScreen) {
     SCREEN = newScreen; // update global
@@ -111,24 +150,11 @@ void changeScreen(byte newScreen){
   }
 }
 
-//void updateTemp(int temp) {
-//  continue;
-//}
-
-void updateSelection(int dir) { // should be done immediately
-  if (SCREEN == 0) { // Standby
-    SEL = displayStandby(SEL, dir);
-  }
-//  else if (SCREEN == 1) { // Heating
-//    continue;
-//  }
-}
-
 /////////////////////////////////////////////////////////////////
 // OLED Display /////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
 
-// display values
+// WRITE SCREEN
 void printScreen(String L1, String L2, String L3, String L4, String L5, String L6, String L7, String L8) {
     oled.clear();
     oled.println(L1);
@@ -141,10 +167,11 @@ void printScreen(String L1, String L2, String L3, String L4, String L5, String L
     oled.println(L8);
 }
 
-// move numeric selection arrow
-int updateSel (int maxSel, int prevSel, int dir) {
+
+// COMPUTE LOCATION (selction with limits)
+int compSelection (int maxSel, int prevSel, int dir) {
     int sel;
-    sel = prevSel + dir;
+    sel = prevSel - dir;
     if (sel > maxSel) {
       sel = maxSel;
     }
@@ -154,14 +181,43 @@ int updateSel (int maxSel, int prevSel, int dir) {
     return(sel);
 }
 
-// standby screen
-int displayStandby(int prevSel, int dir) {
-    // pprevSel <- previous selection
-    // dir <- direction to move selection in
-    // eventually want to return current line selected
-    // should pass values into here
+// REFRESH SCREEN
+void refreshScreen(int dir, int target) { // should be done immediately
+  if (SCREEN == 0) { // STANDBY
+    SEL = standbyScreen(SEL, dir, target);
+  }
+  else if (SCREEN == 1) { // HEATING
+    SEL = heatingScreen(SEL, dir, target);
+  }
+}
+
+// STANDBY SCREEN - Screen 0
+int standbyScreen(int prevSel, int dir, int target) {
     int maxSel = 2; 
-    int selection = updateSel(maxSel, prevSel, dir);
+    int selection = compSelection(maxSel, prevSel, dir);
+    String arrow = String("`");
+    String L1 = String("STATUS: STANDBY");
+    String blank = String("");
+//    String L3 = String("Current temp      000");
+    String L3 = String(target);
+    String L4 = String("Temp target       000");
+    if (selection == 2) {
+        String L6 = String("Start heating       ");
+        String L7 = String("Change Target Temp  " + arrow);   
+        printScreen(L1, blank, L3, L4, blank, L6, L7, blank);
+    }
+    else {
+        String L6 = String("Start heating       " + arrow);
+        String L7 = String("Change Target Temp  ");
+        printScreen(L1, blank, L3, L4, blank, L6, L7, blank);
+    }
+    return(selection);
+}
+
+// HEATING SCREEN - Screen 1
+int heatingScreen(int prevSel, int dir, int target) {
+    int maxSel = 2; 
+    int selection = compSelection(maxSel, prevSel, dir);
     String arrow = String("`");
     String L1 = String("STATUS: STANDBY");
     String blank = String("");
@@ -178,34 +234,6 @@ int displayStandby(int prevSel, int dir) {
         printScreen(L1, blank, L3, L4, blank, L6, L7, blank);
     }
     return(selection);
-}
-
-/////////////////////////////////////////////////////////////////
-// Button Update ////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////
-
-void upPress(Button2& btn) {
-    Serial.println("Up: Pressed");
-    arrow(1);
-}
-
-void selectPress(Button2& btn) {
-    unsigned int time = btn.wasPressedFor();
-    if (time < 1000) {
-        Serial.println("Select: Regular press");
-        select();
-        togglePlatePower();
-    } 
-    else {
-        Serial.println("Select: long press");
-        Serial.println("Turning OFF");
-        setPlatePower(false);
-    }
-}
-
-void downPress(Button2& btn) {
-    Serial.println("Down: Pressed");
-    arrow(-1);
 }
 
 /////////////////////////////////////////////////////////////////
