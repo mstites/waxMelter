2dc0be822893229135e4c336ccd5562dc50c7e5e
mstites <maeve@mstites.com>
2020-12-09 11:39:32 -0500

    Find better starter code

--- software/webUiOnly/webUiOnly.ino
+++ software/webUiOnly/webUiOnly.ino
@@ -1,45 +1,42 @@
 /*********
   Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-thermostat-web-server/
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
   
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-*********/
-
-#include <Arduino.h>
+********/
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-
-AsyncWebServer server(80);
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 // REPLACE WITH YOUR NETWORK CREDENTIALS
 const char* ssid = "GoofZone";
 const char* password = "9139122626";
 
-const char* PARAM_INPUT_1 = "input1";
-const char* PARAM_INPUT_2 = "input2";
-const char* PARAM_INPUT_3 = "input3";
+// Default Threshold Temperature Value
+String inputMessage = "25.0";
+String lastTemperature;
+String enableArmChecked = "checked";
+String inputMessage2 = "true";
 
-// HTML web page to handle 3 input fields (input1, input2, input3)
+// HTML web page to handle 2 input fields (threshold_input, enable_arm_input)
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html><head>
-  <title>ESP Input Form</title>
+  <title>Temperature Threshold Output Control</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   </head><body>
+  <h2>DS18B20 Temperature</h2> 
+  <h3>%TEMPERATURE% &deg;C</h3>
+  <h2>ESP Arm Trigger</h2>
   <form action="/get">
-    input1: <input type="text" name="input1">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input2: <input type="text" name="input2">
-    <input type="submit" value="Submit">
-  </form><br>
-  <form action="/get">
-    input3: <input type="text" name="input3">
+    Temperature Threshold <input type="number" step="0.1" name="threshold_input" value="%THRESHOLD%" required><br>
+    Arm Trigger <input type="checkbox" name="enable_arm_input" value="true" %ENABLE_ARM_INPUT%><br><br>
     <input type="submit" value="Submit">
   </form>
 </body></html>)rawliteral";
@@ -47,6 +44,43 @@ const char index_html[] PROGMEM = R"rawliteral(
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
+
+AsyncWebServer server(80);
+
+// Replaces placeholder with DS18B20 values
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "TEMPERATURE"){
+    return lastTemperature;
+  }
+  else if(var == "THRESHOLD"){
+    return inputMessage;
+  }
+  else if(var == "ENABLE_ARM_INPUT"){
+    return enableArmChecked;
+  }
+  return String();
+}
+
+// Flag variable to keep track if triggers was activated or not
+bool triggerActive = false;
+
+const char* PARAM_INPUT_1 = "threshold_input";
+const char* PARAM_INPUT_2 = "enable_arm_input";
+
+// Interval between sensor readings. Learn more about ESP32 timers: https://RandomNerdTutorials.com/esp32-pir-motion-sensor-interrupts-timers/
+unsigned long previousMillis = 0;     
+const long interval = 5000;    
+
+// GPIO where the output is connected to
+const int output = 2;
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 4;     
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   Serial.begin(115200);
@@ -57,46 +91,75 @@ void setup() {
     return;
   }
   Serial.println();
-  Serial.print("IP Address: ");
+  Serial.print("ESP IP Address: http://");
   Serial.println(WiFi.localIP());
-
-  // Send web page with input fields to client
+  
+  pinMode(output, OUTPUT);
+  digitalWrite(output, LOW);
+  
+  // Start the DS18B20 sensor
+  sensors.begin();
+  
+  // Send web page to client
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
+    request->send_P(200, "text/html", index_html, processor);
   });
 
-  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  // Receive an HTTP GET request at <ESP_IP>/get?threshold_input=<inputMessage>&enable_arm_input=<inputMessage2>
   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputMessage;
-    String inputParam;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    // GET threshold_input value on <ESP_IP>/get?threshold_input=<inputMessage>
     if (request->hasParam(PARAM_INPUT_1)) {
       inputMessage = request->getParam(PARAM_INPUT_1)->value();
-      inputParam = PARAM_INPUT_1;
-    }
-    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
-    else if (request->hasParam(PARAM_INPUT_2)) {
-      inputMessage = request->getParam(PARAM_INPUT_2)->value();
-      inputParam = PARAM_INPUT_2;
-    }
-    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
-    else if (request->hasParam(PARAM_INPUT_3)) {
-      inputMessage = request->getParam(PARAM_INPUT_3)->value();
-      inputParam = PARAM_INPUT_3;
-    }
-    else {
-      inputMessage = "No message sent";
-      inputParam = "none";
+      // GET enable_arm_input value on <ESP_IP>/get?enable_arm_input=<inputMessage2>
+      if (request->hasParam(PARAM_INPUT_2)) {
+        inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+        enableArmChecked = "checked";
+      }
+      else {
+        inputMessage2 = "false";
+        enableArmChecked = "";
+      }
     }
     Serial.println(inputMessage);
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
-                                     + inputParam + ") with value: " + inputMessage +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
+    Serial.println(inputMessage2);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP.<br><a href=\"/\">Return to Home Page</a>");
   });
   server.onNotFound(notFound);
   server.begin();
 }
 
 void loop() {
-  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    sensors.requestTemperatures();
+    // Temperature in Celsius degrees 
+    float temperature = sensors.getTempCByIndex(0);
+    Serial.print(temperature);
+    Serial.println(" *C");
+    
+    // Temperature in Fahrenheit degrees
+    /*float temperature = sensors.getTempFByIndex(0);
+    Serial.print(temperature);
+    Serial.println(" *F");*/
+    
+    lastTemperature = String(temperature);
+    
+    // Check if temperature is above threshold and if it needs to trigger output
+    if(temperature > inputMessage.toFloat() && inputMessage2 == "true" && !triggerActive){
+      String message = String("Temperature above threshold. Current temperature: ") + 
+                            String(temperature) + String("C");
+      Serial.println(message);
+      triggerActive = true;
+      digitalWrite(output, HIGH);
+    }
+    // Check if temperature is below threshold and if it needs to trigger output
+    else if((temperature < inputMessage.toFloat()) && inputMessage2 == "true" && triggerActive) {
+      String message = String("Temperature below threshold. Current temperature: ") + 
+                            String(temperature) + String(" C");
+      Serial.println(message);
+      triggerActive = false;
+      digitalWrite(output, LOW);
+    }
+  }
 }
